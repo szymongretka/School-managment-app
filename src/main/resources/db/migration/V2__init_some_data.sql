fc386bcca4c82cf5ffcea4194ba95d4c068e30f4
szymongretka <szymon.gretka@gmail.com>
2024-02-18 21:55:19 +0100

    Fix flyway init data

--- src/main/resources/db/migration/V2__init_some_data.sql
+++ src/main/resources/db/migration/V2__init_some_data.sql
@@ -17,9 +17,9 @@ VALUES (3, 'Dziecko3', 'Nazwisko2', 2, 1);
 
 INSERT INTO ATTENDANCE(child_id, entry_date, exit_date)
 VALUES (1, '2024-02-15 07:01:00.000000', '2024-02-15 11:00:00.000000');
-INSERT INTO ATTENDANCE(id, child_id, entry_date, exit_date)
+INSERT INTO ATTENDANCE(child_id, entry_date, exit_date)
 VALUES (1, '2024-02-18 05:05:00.000000', '2024-02-18 16:13:00.000000');
-INSERT INTO ATTENDANCE(id, child_id, entry_date, exit_date)
+INSERT INTO ATTENDANCE(child_id, entry_date, exit_date)
 VALUES (2, '2024-02-18 05:05:00.000000', '2024-02-18 16:13:00.000000');
-INSERT INTO ATTENDANCE(id, child_id, entry_date, exit_date)
+INSERT INTO ATTENDANCE(child_id, entry_date, exit_date)
 VALUES (3, '2024-02-18 05:05:00.000000', '2024-02-18 16:13:00.000000');
